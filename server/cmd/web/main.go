c75b471cb1738a8a3fafa9039725303e9f536a9f
mcgigglepop <christoph.alano@gmail.com>
2025-11-04 02:18:30 -0600

    setup redis

--- server/cmd/web/main.go
+++ server/cmd/web/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/gob"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/alexedwards/scs/redisstore"
 	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
+
 	appConfig "github.com/mcgigglepop/tcg-marketplace/server/internal/config"
 )
 
@@ -12,6 +20,16 @@ const portNumber = ":80"
 
 var app appConfig.AppConfig
 var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
+
+func getEnvOrExit(key string) string {
+	val := os.Getenv(key)
+	if val == "" {
+		log.Fatalf("Missing required env var: %s", key)
+	}
+	return val
+}
 
 func main() {
 	// Initialize application
@@ -32,8 +50,83 @@ func main() {
 
 // run initializes the application config, session, AWS clients, and templates
 func run() error {
+	gob.Register(map[string]int{})
+	inProduction := flag.Bool("production", true, "Application is in production")
+	useCache := flag.Bool("cache", true, "Use template cache")
 
+	cognitoUserPoolID := flag.String(
+		"cognito-user-pool-id",
+		os.Getenv("COGNITO_USER_POOL_ID"),
+		"Cognito user pool ID",
+	)
+
+	cognitoClientID := flag.String(
+		"cognito-client-id",
+		os.Getenv("COGNITO_CLIENT_ID"),
+		"Cognito app client ID",
+	)
+
+	// parse flags
+	flag.Parse()
+
+	if *cognitoUserPoolID == "" || *cognitoClientID == "" {
+		fmt.Println("Missing Cognito flags")
+		os.Exit(1)
+	}
+
+	app.InProduction = *inProduction
+	app.UseCache = *useCache
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.InfoLog = infoLog
+	app.ErrorLog = errorLog
+
+	// Redis connection setup using redigo
+	redisEndpoint := getEnvOrExit("REDIS_ENDPOINT") // format: host:port
+	redisPassword := os.Getenv("REDIS_PASSWORD")    // optional
+
+	// Create a redigo connection pool
+	pool := &redis.Pool{
+		MaxIdle:     10,
+		MaxActive:   100,
+		IdleTimeout: 240 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			opts := []redis.DialOption{}
+			if redisPassword != "" {
+				opts = append(opts, redis.DialPassword(redisPassword))
+			}
+			c, err := redis.Dial("tcp", redisEndpoint, opts...)
+			if err != nil {
+				return nil, fmt.Errorf("failed to connect to Redis: %v", err)
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+
+	// Test connection immediately
+	conn := pool.Get()
+	defer conn.Close()
+	if _, err := conn.Do("PING"); err != nil {
+		log.Fatalf("failed to connect to Redis: %v", err)
+	}
+	infoLog.Println("Connected to Redis for session storage")
+
+	// set up session
 	session = scs.New()
+	session.Store = redisstore.New(pool)
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
 	app.Session = session
+
 	return nil
 }
--- server/go.mod
+++ server/go.mod
@@ -7,3 +7,9 @@ require (
 	github.com/go-chi/chi v1.5.5
 	github.com/justinas/nosurf v1.1.1
 )
+
+require (
+	github.com/alexedwards/scs/redisstore v0.0.0-20250417082927-ab20b3feb5e9 // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
+	github.com/gomodule/redigo v1.9.2 // indirect
+)
