6c091a04cbdf9f1cfd55eae2e870bcc188a87984
mcgigglepop <christoph.alano@gmail.com>
2025-11-06 00:09:18 -0600

    added forms templatedata

--- server/go.mod
+++ server/go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/go-chi/chi v1.5.5
 	github.com/gomodule/redigo v1.9.2
 	github.com/justinas/nosurf v1.1.1
+		github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
 )
 
 require (
---
+++ server/internal/forms/forms.go
@@ -0,0 +1,82 @@
+package forms
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/asaskevich/govalidator"
+)
+
+// Form creates a custom form struct, embeds a url.Values object
+type Form struct {
+	url.Values
+	Errors errors
+}
+
+// New initializes a form struct
+func New(data url.Values) *Form {
+	return &Form{
+		data,
+		errors(map[string][]string{}),
+	}
+}
+
+// Valid returns true if there are no errors, otherwise false
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
+// Required checks for required fields
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+		}
+	}
+}
+
+// Has checks if form field is in post and not empty
+func (f *Form) Has(field string) bool {
+	x := f.Get(field)
+	if x == "" {
+		return false
+	}
+	return true
+}
+
+// MinLength checks for string minimum length
+func (f *Form) MinLength(field string, length int) bool {
+	x := f.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
+		return false
+	}
+	return true
+}
+
+// IsEmail checks for valid email address
+func (f *Form) IsEmail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "Invalid email address")
+	}
+}
+
+// IsNumber checks if the field is a number
+func (f *Form) IsNumber(field string) {
+	if !govalidator.IsNumeric(f.Get(field)) {
+		f.Errors.Add(field, "Invalid number")
+	}
+}
+
+func (f *Form) GetInt(field string) int {
+	val := f.Get(field)
+	i, err := strconv.Atoi(val)
+	if err != nil {
+		// Optional: log error or return default
+		return 0
+	}
+	return i
+}
