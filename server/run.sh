57592dd66448f860d722945e484a158bd999348d
mcgigglepop <christoph.alano@gmail.com>
2025-11-08 16:48:36 -0600

    modify to use .env

--- server/run.sh
+++ server/run.sh
@@ -2,11 +2,18 @@
 
 set -euo pipefail
 
+if [ -f .env ]; then
+  echo "Loading environment variables from .env"
+  set -a
+  source .env
+  set +a
+fi
+
 GO_BUILD_OUTPUT="tcg-marketplace-build"
-CACHE_FLAG="false"
-PRODUCTION_FLAG="false"
-COGNITO_USER_POOL_ID=""
-COGNITO_CLIENT_ID=""
+CACHE_FLAG=${CACHE_FLAG:-false}
+PRODUCTION_FLAG=${PRODUCTION_FLAG:-false}
+COGNITO_USER_POOL_ID=${COGNITO_USER_POOL_ID:?Missing COGNITO_USER_POOL_ID}
+COGNITO_CLIENT_ID=${COGNITO_CLIENT_ID:?Missing COGNITO_CLIENT_ID}
 
 if [ ! -d "cmd/web" ]; then
   echo "Error: script must be run from repo root or server directory" >&2
